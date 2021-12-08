44f9fccf05643e3412d4158d06783300e5413945
Pavel Andonov <god971@abv.bg>
2021-12-09 01:48:49 +0200

    Card component, some test cards on home page

--- client/TechOutletApp/src/app/app.module.ts
+++ client/TechOutletApp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { NavComponent } from './nav/nav.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    NavComponent
+    NavComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
